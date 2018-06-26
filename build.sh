b081a71cdf69a168ac39ca5e73027becc85896f9
Nathan Barber <nathanbarber6@gmail.com>
2018-06-26 19:33:35 -0400

    First commit

---
+++ build.sh
@@ -0,0 +1,2 @@
+sudo cp minnode /usr/bin
+sudo ln -s minnode/minnode.js mnode
---
+++ minnode/minnode.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const express = require("express");
+var app = express();
+app.use(express.static(`.`))
+app.listen(8080);
